46d526526cd2ca4fb10f8b64decbe4fee49ea734
Muhammad Shahmeer <mshahmeeramjad@gmail.com>
2023-07-27 16:31:54 -0700

    Added word count feature and lower case button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,6 +11,10 @@ export default function (props) {
     let newText = text.toUpperCase();
     setText(newText);
   }
+  const handlelowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
   
   const [text, setText] = useState('Enter text here');
 
@@ -18,15 +22,19 @@ export default function (props) {
     <>
     <div className='container'>
         <h1>{props.heading}</h1>
-<div className="mb-3">
+<div className="mb-4">
  
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-success mx-1 my-1"  onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-dark mx-1 my-1" onClick={handlelowClick}>Convert to Lowercase</button>
     </div>
     <div className="container my-3">
       <h1>Your text Summary</h1>
       <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
     </div>
     </>
   )
