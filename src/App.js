9282a025581bb1e5acf0c19c070943f04e42ee81
Muhammad Shahmeer <mshahmeeramjad@gmail.com>
2023-07-30 00:47:27 -0700

    Enabled Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import Navbar from './components/Navbar';
-// import About from './components/About';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
 function App() {
+  const [mode, setmode] = useState('light');
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setmode('dark');
+    }
+    else{
+      setmode('light');
+    }
+  }
   return (
     <>
-<Navbar title="TextUtils"/>
+<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
 <div className="container my-3">
   <TextForm heading = "Enter the text to analyze"/>
-  {/* <About/> */}
 
   </div>
 
     </>
   );
 }
-
 export default App;
